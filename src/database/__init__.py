fb007592e86ef1cb622897c93aa160de4f289bbb
RomanSapunGit <sapunroma23@gmail.com>
2025-10-09 18:43:42 +0200

    feat(base): add base.py
    Added Base class which inherits DeclarativeBase and also provides default order_by for future models.

---
+++ src/database/__init__.py
@@ -0,0 +1,21 @@
+from database.models.base import Base
+from database.models.order_models import (
+    OrderModel
+)
+from database.models.movie_models import (
+    GenreModel,
+    DirectorModel,
+    StarModel,
+    CertificationModel,
+    MovieModel
+)
+from database.models.payment_models import (
+    PaymentModel,
+)
+from database.models.shopping_cart_models import (
+    CartModel,
+)
+from database.models.user_models import (
+    UserModel,
+    UserGroupModel
+)
