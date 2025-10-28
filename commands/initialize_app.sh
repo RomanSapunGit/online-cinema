b532e28c4031ffa7fb6143d5e3db0fc16b5a24c6
comfy <sapunroma23@gmail.com>
2025-10-28 20:52:44 +0100

    chore(command): add commands to use for docker compose

---
+++ commands/initialize_app.sh
@@ -0,0 +1,8 @@
+set -e
+
+echo "Running Alembic migrations..."
+alembic upgrade head
+
+echo "Starting FastAPI..."
+
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload --reload-dir /usr/src/fastapi
