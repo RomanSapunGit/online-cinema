83905b597f72a0a58779e401b933332e6ec54baf
comfy <sapunroma23@gmail.com>
2025-10-28 20:25:43 +0100

    chore(docker compose): update docker compose

---
+++ src/schemas/__init__.py
@@ -0,0 +1,19 @@
+from schemas.users import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+    UserActivationRequestSchema,
+    MessageResponseSchema,
+    PasswordResetRequestSchema,
+    PasswordResetCompleteRequestSchema,
+    UserLoginResponseSchema,
+    UserLoginRequestSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema
+)
+from schemas.movies import (
+    MovieDetailSchema,
+    MovieListResponseSchema,
+    MovieListItemSchema,
+    MovieCreateSchema,
+    MovieUpdateSchema
+)
