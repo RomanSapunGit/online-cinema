d13a2f62fac14482e9c77500898146882e2c7839
comfy <sapunroma23@gmail.com>
2025-10-28 01:57:36 +0100

    feat(config settings): update settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,17 +1,50 @@
 import os
+from pathlib import Path
 
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class BaseAppSettings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).parent.parent
+
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+    NOTIFICATION_EMAIL_TEMPLATE_NAME: str = "notification.html"
+    PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "tests" / "seeds" / "test_data.csv")
+
+    PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "theater.db")
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
+    SENDGRID_API_KEY: str = os.getenv("SENDGRID_API_KEY", "1234")
+    EMAIL_SENDER: str = os.getenv("EMAIL_SENDER", "dummy@dummy-domain.com")
+
+    FRONTEND_URL: str = os.getenv("FRONTEND_URL", "http://localhost:8000")
+    STRIPE_API_KEY: str = os.getenv("STRIPE_API_KEY", "")
+    STRIPE_WEBHOOK_SECRET: str = os.getenv("STRIPE_WEBHOOK_SECRET", "")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
+
+    REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost")
+
+    LOGIN_TIME_DAYS: int = 7
+
+
+class TestingSettings(BaseAppSettings):
+    SECRET_KEY_ACCESS: str = "SECRET_KEY_ACCESS"
+    SECRET_KEY_REFRESH: str = "SECRET_KEY_REFRESH"
+    JWT_SIGNING_ALGORITHM: str = "HS256"
+
 
 class Settings(BaseAppSettings):
-    pass
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
+    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
+    JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
